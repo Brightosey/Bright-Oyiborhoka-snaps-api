c3397881abe009c7acf8aaca51e47949b7ccc49f
Brightosey <Brightosey@users.noreply.github.com>
2025-04-06 01:02:24 -0400

    modified index.js and my env file

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const PORT = process.env.PORT || 5050;
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
 
 // Middleware
-app.use(cors());
+app.use(cors({ origin: "https://bright-oyiborhoka-snaps.vercel.app" }));
 app.use(express.json());
 app.use(express.static("public"));
 
